53afe16c38b09c990c3d849ce84ac92d02f656b4
Erol <lakmerol@gmail.com>
2023-01-17 18:59:20 +0300

    Beginner_2 Added

---
+++ HackerRank_Beginner_Question_2/Answer/PlusMinus.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Answer
+{
+
+    internal  class PlusMinus
+    {
+        public  List<decimal> Calculator(List<int> arr)
+        {
+            decimal n = 0; decimal z=0; decimal p=0;
+
+            List<decimal> result = new List<decimal>();
+
+           
+            for (int i = 0; i < arr.Count; i++)
+            {
+               
+                if (arr[i] < 0) n++;
+                if(arr[i] > 0) p++;
+                if(arr[i] == 0) z++;
+               
+
+
+
+
+            }
+
+            decimal NegatifRate = Math.Round(n / arr.Count, 5);
+            decimal PosittiveRate = Math.Round(p / arr.Count, 5);
+            decimal Zero = Math.Round(z / arr.Count, 5);
+            result.Add(NegatifRate);
+            result.Add(Zero);
+            result.Add(PosittiveRate);
+            return result;
+        }
+
+       
+    }
+}
---
+++ HackerRank_Beginner_Question_2/Answer/Program.cs
@@ -0,0 +1,18 @@
+﻿
+
+using Answer;
+
+PlusMinus plusMinus = new PlusMinus();
+
+List<int> vsd = new List<int>()
+{
+
+    1, 2, 3, 4, 5, 6, 7,0,0,-2,-1,-3,-5
+
+
+};
+List<decimal> vs = plusMinus.Calculator(vsd);
+Console.WriteLine(vs[0]);
+Console.WriteLine(vs[1]);
+Console.WriteLine(vs[2]);
+Console.ReadLine();
