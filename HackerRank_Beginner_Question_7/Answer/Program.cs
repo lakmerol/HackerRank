f03a2cf6f17fc0b687d2e3fb06f8d0e6fd80deaa
Erol <lakmerol@gmail.com>
2023-01-23 20:41:46 +0300

    Beginner_Question_7 Added

---
+++ HackerRank_Beginner_Question_7/Answer/Program.cs
@@ -0,0 +1,14 @@
+﻿static string kangaroo(int x1, int v1, int x2, int v2)
+{
+    //starting positions conditions
+    if ((x1 < x2 && v1 < v2)
+        || (x1 > x2 && v1 > v2)
+        || (x1 != x2 && v1 == v2))
+    {
+        return "NO";
+    }
+    else if ((x1 - x2) % (v2 - v1) == 0)
+        return "YES";
+    else
+        return "NO";
+}
