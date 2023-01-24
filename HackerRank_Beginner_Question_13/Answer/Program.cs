7514c98a27b8bf880ca53662881fb8bcefd24f72
Erol <lakmerol@gmail.com>
2023-01-24 18:42:01 +0300

    Beginner_Question_13 Added

---
+++ HackerRank_Beginner_Question_13/Answer/Program.cs
@@ -0,0 +1,8 @@
+﻿static string bonAppetit(List<int> bill, int k, int b)
+{
+    bill.RemoveAt(k);
+    int Account = bill.Sum() / 2;
+    if (Account == b) return "Bon Appetit";
+    return (b - Account).ToString();
+}
+
