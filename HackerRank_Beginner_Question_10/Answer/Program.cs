0992c0cf84e8b0b5226d79fb8f319be62c882223
Erol <lakmerol@gmail.com>
2023-01-24 10:10:48 +0300

    Beginner_Question_10 Added

---
+++ HackerRank_Beginner_Question_10/Answer/Program.cs
@@ -0,0 +1,31 @@
+﻿ static int divisibleSumPairs(int n, int k, List<int> ar)
+{
+    int count = 0;
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            int sum = ar[i] + ar[j];
+            if ((sum % k) == 0)
+            {
+                count++;
+
+
+
+            }
+
+        }
+
+    }
+    return count;
+}
+    int n = 6;
+int k = 3;
+List<int> ar = new List<int>()
+{
+    1, 3, 2, 6, 1, 2
+
+};
+int counter = divisibleSumPairs(n, k, ar);
+Console.WriteLine(counter);
+Console.ReadLine();
