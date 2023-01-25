a8a411a2ad9dde49b97ef769475bd0a176d4eb8a
Erol <lakmerol@gmail.com>
2023-01-25 23:12:37 +0300

    Beginner_Question_15 Added

---
+++ HackerRank_Beginner_Question_15/Answer/Program.cs
@@ -0,0 +1,14 @@
+﻿ static int pageCount(int n, int p)
+{
+        if (n / p < 1.5)
+        {
+            if ((n - p) / 2 < 1) return 0;
+            return (n - p) / 2;
+        }
+
+        return p / 2;
+}
+
+int d = pageCount(6, 5);
+Console.WriteLine(d);
+Console.ReadLine();
