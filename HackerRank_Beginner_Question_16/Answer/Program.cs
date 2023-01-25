d30b72e9eba9c0459a992cdf3402aa3b9ce1cc62
Erol <lakmerol@gmail.com>
2023-01-26 00:51:49 +0300

    Beginner_Question_16 Added

---
+++ HackerRank_Beginner_Question_16/Answer/Program.cs
@@ -0,0 +1,32 @@
+﻿static int countingValleys(int steps, string path)
+{
+    char[] pathv = path.ToCharArray();
+    int sealevel = 0; int counter = 0;
+ while (steps > 0)
+    {
+
+       if (pathv[0] == 'D') sealevel--;
+       else sealevel++;
+        pathv = pathv.Skip(1).ToArray();
+        steps--;
+       if (sealevel == -1)
+        {
+            if (pathv[0] == 'D') sealevel--;
+            else sealevel++;
+            pathv = pathv.Skip(1).ToArray();
+
+            steps--;
+            if (sealevel == 0)
+            {
+                counter++;
+            }
+
+        }
+
+    }
+    return counter;
+}
+
+int i = countingValleys(8, "UDDDUDUU");
+Console.WriteLine(i);
+Console.ReadLine();
